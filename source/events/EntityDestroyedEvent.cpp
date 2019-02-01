75a6b2902fab4123683f5cb3e81769862e01e36f
Alex <alex.koukoulas@king.com>
2019-02-01 16:59:07 +0000

    Added entity destruction event

---
+++ source/events/EntityDestroyedEvent.cpp
@@ -0,0 +1,8 @@
+//
+//  EntityDestroyedEvent.cpp
+//  Hardcore2D
+//
+//  Created by Alex Koukoulas on 01/02/2019.
+//
+
+#include "EntityDestroyedEvent.h"
---
+++ source/events/EntityDestroyedEvent.h
@@ -0,0 +1,13 @@
+//
+//  EntityDestroyedEvent.h
+//  Hardcore2D
+//
+//  Created by Alex Koukoulas on 01/02/2019.
+//
+
+#ifndef EntityDestroyedEvent_h
+#define EntityDestroyedEvent_h
+
+
+
+#endif /* EntityDestroyedEvent_h */
